fde5c9a29c1743ae140f4752b47569be4fc46102
maddparker <maddparker@95bea282-5f47-11de-92c7-cf56be1892c0>
2011-04-24 22:18:47 +0000

    Accounts for new locations of Processing native libs

--- trunk/proclipsing.core/src/proclipsing/processingprovider/ProcessingLibrary.java
+++ trunk/proclipsing.core/src/proclipsing/processingprovider/ProcessingLibrary.java
@@ -48,16 +48,46 @@ public class ProcessingLibrary {
      * @param files
      * @return
      */
+    
+    private static final String[] newNativeLibDirs = new String[]{
+    	"linux32",
+    	"linux64",
+    	"macosx",
+    	"opengl.jar",
+    	"windows32",
+    	"windows64"};
+    
     private URL[] getUrls(File[] files) {
         ArrayList<URL> urls = new ArrayList<URL>();
+        ArrayList<URL> nativeLibs = new ArrayList<URL>();
         for(File file : files) {
-            if (file.isDirectory()) continue;
+            if (file.isDirectory()){
+            	boolean isNativeLib = false;
+            	for(String nativeLibDir: newNativeLibDirs){
+            		isNativeLib = isNativeLib || file.getAbsolutePath().endsWith(nativeLibDir);
+            	}
+            	
+            	if(isNativeLib){
+            		for(File nativeLib: file.listFiles()){
+            			try{
+            				nativeLibs.add(nativeLib.toURI().toURL());	
+            			} catch (MalformedURLException e) {
+                            LogHelper.LogError(e);
+                        }
+            		}
+            	} else {
+            		continue;
+            	}
+            }
             try {
                 urls.add(file.toURI().toURL());
             } catch (MalformedURLException e) {
                 LogHelper.LogError(e);
             }
         }
+        
+        urls.addAll(nativeLibs);
+        
         return urls.toArray(new URL[urls.size()]);
     }
     
